cf0354dd3a0570e936de84a082840ce0948c08a2
Enrique <enriquepadilla1999@gmail.com>
2025-05-18 18:43:07 +0200

    Para practicar sesión 9 y revisada

---
+++ PRACTICA_9/para_practicar_9.py
@@ -0,0 +1,18 @@
+# # Algoritmia
+# ## Práctica 9
+# En esta práctica se resolverá el problema de Subsecuencia Común Más Larga (LCS) y el problema de la mochila 1-0.
+
+
+# Subsecuencia Común Más Larga (LCS)
+
+def es_subsecuencia(subsecuencia, secuencia):
+    pass
+
+def subsecuencia_comun_mas_larga(x, y):
+    pass
+
+def subsecuencias_comunes_mas_largas(x, y):
+    pass
+
+def mochila(objetos, capacidad):
+    pass
